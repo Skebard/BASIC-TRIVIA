1a2540d67d408061b15e9787d01966fa6be0a15f
Skebard <49041487+Skebard@users.noreply.github.com>
2020-09-13 18:39:24 +0300

    Code refacotoring
    The code has been refactored in order to get a more readable code and using OOP.
    The main functionalities are implemented.
    To do (optional):
    -add questions counter
    -add a scoring system
    -add users
    -store data on localStorage

--- js/script.js
+++ js/script.js
@@ -1,47 +1,75 @@
 const BASE_URL = "https://opentdb.com/";
-const DEF_NUM_QUE =10; //default number of questions for quiz
+const DEF_NUM_QUE = 10; //default number of questions for quiz
+const SHOW_ANSWER_TIME = 1000; //milliseconds after choosing an answer
 
 let token; // the token is used to avoid getting repeated questions
 let categories;
-let url = BASE_URL +"api_category.php";
+let url = BASE_URL + "api_category.php";
 console.log("\n hi\n");
 let categoriesContainer = document.getElementById("categories-id");
 let gameSettings = document.getElementById("game-settings-id");
+let gameBoard = document.querySelector(".game-board");
+let gameMenu = document.getElementById("game-menu-id");
+let answersHTML = Array.from(document.getElementById("answers-id").children);
+let questionHTML = document.getElementById("question-id");
+let endGameHTML = document.getElementById("end-game-screen-id");
+let mainMenuBtn = document.getElementById("main-menu-btn-id");
 updateToken();
 displayLoading();
 getAllCategories()
-    .then((data)=>createCategoryBtns(data))
-    .then(hideLoading);
+    .then((data) => createCategoryBtns(data))
+    .then(hideLoading)
+    .then(() => {
+        gameSettings.addEventListener("submit", (e) => {
+            e.preventDefault();
+            if (validateGameSettings){
+                let number = parseInt(gameSettings.elements.number.value);
+                let category = categoriesContainer.querySelector(".selected").id.slice(9);
+                let difficulty = gameSettings.elements.difficulty.value.toLowerCase();
+                console.log(number);
+                console.log(category);
+                console.log(difficulty);
+                let newGame = new Game(number,category,difficulty);
+                newGame.startGame();
+            }
+        });
+    });
+    mainMenuBtn.addEventListener("click",()=>{
+        endGameHTML.classList.add("hide");
+        gameMenu.classList.remove("hide");
+    });
 //click
+console.log("run")
 
-gameSettings.addEventListener("submit",(e)=>{
-    e.preventDefault();
-    if(validateGameSettings) startGame();
-});
+
+
 
 
 
 //Function to get the id and name of the categories in an array
-function getAllCategories(){
-    let categoriesURL = BASE_URL+"api_category.php";
+function getAllCategories() {
+    let categoriesURL = BASE_URL + "api_category.php";
     return fetch(categoriesURL)
-        .then(response=>response.json())
-        .then((data)=>{
+        .then(response => response.json())
+        .then((data) => {
             return data.trivia_categories;
         });
 }
 
-function createCategoryBtns(data){
-    let any = {id:"any",name:"All categories"};
+function createCategoryBtns(data) {
+    let any = {
+        id: "any",
+        name: "All categories"
+    };
     data.unshift(any);
-    data.forEach((category)=>{
+    data.forEach((category) => {
         let btn = document.createElement("button");
-        btn.id = "category-"+category.id;
+        btn.id = "category-" + category.id;
         let title = document.createElement("span");
         title.textContent = category.name;
         btn.appendChild(title);
         btn.classList.add("category-btn");
-        btn.addEventListener("click",()=>{
+        btn.addEventListener("click", () => {
             categoriesContainer.querySelector(".selected").classList.remove("selected");
             btn.classList.add("selected");
         });
@@ -51,74 +79,175 @@ function createCategoryBtns(data){
 }
 
 
-function startGame(id){
-    gameSettings.classList.add("hide");
-    categoriesContainer.classList.add("hide");
-    displayLoading();
-    let number = gameSettings.querySelector("input[type='number'").value;
-    let categoryId = categoriesContainer.querySelector(".selected").id.slice(9);//slice to remove "category-" from the id
-    let difficulty = gameSettings.querySelector("#difficulty-id > [selected]").textContent.toLowerCase();
-    getQuestions(number,categoryId,difficulty,"multiple")
-        .then((data)=>{
-            hideLoading();
-            displayQuestions(data);
-        });
-    //displayQuestions board
-    //update used questions check token
+
+
+
+// max exclusive
+function randomInt(min, max) {
+    return min + Math.floor(Math.random() * (max - min));
 }
 
-function questionsDisplay(data){
-    document.getElementById("answers-id");
-
-    console.log("diplay");
-}
-
-function hideLoading(){
+function hideLoading() {
     document.getElementById("loading-animation").classList.add("hide");
-
+    console.log("byebye");
 }
-function displayLoading(){
+
+function displayLoading() {
     document.getElementById("loading-animation").classList.remove("hide");
 }
-function updateToken(){
-    let requestTokenUrl=BASE_URL+"api_token.php?command=request";
+
+function updateToken() {
+    let requestTokenUrl = BASE_URL + "api_token.php?command=request";
     fetch(requestTokenUrl)
-        .then((response)=>response.json())
-        .then((data)=>{
-            token = "token="+data.token;
+        .then((response) => response.json())
+        .then((data) => {
+            token = "token=" + data.token;
         });
 }
 
-/**
- * 
- * @param {int} number  number of requested questions
- * @param {int} categoryId each category has assigned and id
- * @param {string} difficulty "easy", "medium" or "difficult"
- * @param {string} type  "multiple" or "boolean"
- * @retuns {array}
- */
-function getQuestions(number=DEF_NUM_QUE,categoryId="any",difficulty="any",type="any"){
-    let selectedCategory = (categoryId==="any")? "":`category=${categoryId}`;
-    let selectedDifficulty = (difficulty==="any")?"": `difficulty=${difficulty}`;
-    let selectedType = (type==="any")?"":`type=${type}`;
-    let requestURL = BASE_URL + `api.php?amount=${number}&${selectedCategory}&${selectedDifficulty}&${selectedType}&${token}`;
-    console.log(requestURL);
-    return fetch(requestURL)
-    .then(response=>response.json())
-    .then((data)=>{
-        console.log(data.results);
-        return data.results;
-    });
-}
 
-function validateGameSettings(){
-    let erroMessage = docuemnt.getElementById("error-message-id");
+function validateGameSettings() {
+    let errorMessage = docuemnt.getElementById("error-message-id");
     let numQuestions = gameSettings.elements[0].value;
-    if(numQuestions < 10 || numQuestions > 30){
+    if (numQuestions < 10 || numQuestions > 30) {
         //print invalid number
         errorMessage.classList.remove("hide");
         return false;
     }
     errorMessage.classList.add("hide");
     return true;
+}
+
+
+
+
+function Game(quantity, category, difficulty, user = false) {
+    let parser = new DOMParser();
+    let questions;
+    let currentQuestion = 0;
+    let rightAnsPosition;
+    let correctAnswers = 0;
+
+    function getQuestions(number = DEF_NUM_QUE, categoryId = "any", difficulty = "any", type = "multiple") {
+        let selectedCategory = (categoryId === "any") ? "" : `category=${categoryId}`;
+        let selectedDifficulty = (difficulty === "any") ? "" : `difficulty=${difficulty}`;
+        let selectedType = `type=${type}`;
+        let requestURL = BASE_URL + `api.php?amount=${number}&${selectedCategory}&${selectedDifficulty}&${selectedType}&${token}`;
+        console.log(requestURL);
+        return fetch(requestURL)
+            .then(response => response.json())
+            .then((data) => {
+                console.log(data.results);
+                return data.results;
+            });
+    }
+
+    function displayQuestions(data) {
+        gameBoard.classList.remove("hide");
+        printAnswers(0);
+        printQuestion(0);
+
+        answersHTML.forEach((ans) => {
+            ans.addEventListener("click", answersEvent);
+        });
+    }
+
+    function nextQuestion() {
+        printAnswers(currentQuestion);
+        printQuestion(currentQuestion);
+    }
+
+    function printAnswers(position) {
+        rightAnsPosition = randomInt(0, 4);
+        let incorrectAns = 0;
+        console.log(questions);
+        console.log(position);
+        answersHTML.forEach((ans, index) => {
+            if (index === rightAnsPosition) {
+                ans.textContent = questions[position].correct_answer;
+            } else {
+                ans.textContent = questions[position].incorrect_answers[incorrectAns];
+                incorrectAns++;
+            }
+            ans.classList = "";
+            console.log(ans);
+        });
+    }
+
+    function printQuestion(position) {
+        let text = convertToText(questions[position].question);
+        questionHTML.innerHTML = text;
+    }
+
+
+    function answersEvent(e) {
+        showAnswers(e.currentTarget);
+        currentQuestion++;
+        if (currentQuestion === quantity) { //-1 cause the array index starts by 0
+            console.log("End game");
+            setTimeout(endGame,SHOW_ANSWER_TIME);
+            return true;
+        }
+        
+        setTimeout(nextQuestion, SHOW_ANSWER_TIME);
+    }
+
+    function showAnswers(clickedAnswer) {
+        console.log(clickedAnswer);
+        if (parseInt(clickedAnswer.dataset.pos) === rightAnsPosition) {
+            clickedAnswer.classList.add("correct");
+            correctAnswers++;
+            return true;
+        }
+        answersHTML.forEach((ans, index) => {
+            if (index === rightAnsPosition) {
+                ans.classList.add("correct");
+            } else {
+                ans.classList.add("incorrect");
+            }
+        });
+
+    }
+
+    function removeEvents() {
+        answersHTML.forEach((ans) => {
+            ans.removeEventListener("click", answersEvent);
+        });
+    }
+
+    this.startGame = async function () {
+        gameMenu.classList.add("hide");
+        displayLoading();
+        questions = await getQuestions(quantity, category, difficulty, "multiple");
+        hideLoading();
+        displayQuestions();
+    }
+
+
+     function endGame() {
+        removeEvents();
+        gameBoard.classList.add("hide");
+        endGameHTML.classList.remove("hide");
+        endGameHTML.querySelector("#points-id").textContent = correctAnswers;
+        correctAnswers=0;
+        currentQuestion=0;
+        updateToken();
+    }
+    this.showValues = function(){
+        console.log(correctAnswers);
+        console.log(currentQuestion);
+    }
+    this.disQuestions = function () {
+        console.log(questions);
+    }
+    this.quest = displayQuestions;
+    this.remov = removeEvents;
+
+
+}
+
+function convertToText(text){
+    let tempoDom = document.createElement("div");
+    tempoDom.innerHTML = text;
+    return tempoDom.textContent;
 }
